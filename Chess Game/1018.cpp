660056eaed3c3b305c74f5930911bf6a47614347
Lee Sun Hong <nuseehr@naver.com>
2019-08-25 23:49:59 +0900

    Update Chess Game

--- Chess Game/1018.cpp
+++ Chess Game/1018.cpp
@@ -4,39 +4,10 @@
 using namespace std;
 
 int board[51][51] = {0, };
-int answer = 0;
-
-void row_check(int M, int N) {
-    
-    for (int i=0; i<M; i++) {
-        for (int j=0; j<N; j++) {
-            if (board[i][j] == board[i][j+1]) {
-                board[i][j+1] = !board[i][j];
-                col_check(M, N);
-                answer++;
-            }
-        }
-    }
-    
-}
-
-void col_check(int M, int N) {
-    
-    for (int i=0; i<M; i++) {
-        for (int j=0; j<N; j++) {
-            if (board[i][j] == board[i+1][j]) {
-                board[i+1][j] = !board[i][j];
-                row_check(M, N);
-                answer++;
-            }
-        }
-    }
-    
-}
 
 int main() {
     int M, N;
-    
+    int answer = 64;
     cin >> M >> N;
     
     for (int i=0; i<M; i++) {
@@ -47,14 +18,33 @@ int main() {
         }
     }
     
-    row_check(M, N);
-    
-    cout << "- - - - - - -" << endl;
-    
-    for (int i=0; i<M; i++) {
-        for (int j=0; j<N; j++) {
-            cout << board[i][j];
+    for (int i=0; i<M-7; i++) {
+        for (int j=0; j<N-7; j++) {
+            int w=0, b=0;
+            
+            for (int k=0; k<8; k++) {
+                for (int l=0; l<8; l++) {
+                    if ((k+l) % 2 == 0 && board[i+k][j+l] != 1) {
+                        w++;
+                    }
+                    else if ((k+l) % 2 == 1 && board[i+k][j+l] != 0) {
+                        w++;
+                    }
+                    if ((k+l) % 2 == 0 && board[i+k][j+l] != 0) {
+                        b++;
+                    }
+                    else if ((k+l) % 2 == 1 && board[i+k][j+l] != 1) {
+                        b++;
+                    }
+                }
+            }
+            if (w < b && w < answer) {
+                answer = w;
+            }
+            else if (b <= w && b < answer) {
+                answer = b;
+            }
         }
-        cout << endl;
     }
+    cout << answer << endl;
 }
