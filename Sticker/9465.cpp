1bff8102515c8ca1d6a7626ed7c3b06c6d65a195
Lee Sun Hong <nuseehr@naver.com>
2019-09-01 00:18:21 +0900

    Update Sticker problem

--- Sticker/9465.cpp
+++ Sticker/9465.cpp
@@ -15,16 +15,19 @@ int main() {
         cin >> N;
         
         for (int i=0; i<2; i++) {
-            for (int j=0; j<N; j++) {
+            for (int j=1; j<=N; j++) {
                 cin >> arr[i][j];
             }
         }
         
-        dp[0][0] = arr[0][0];
         dp[0][1] = arr[0][1];
-     
-        for (int i=0; i<N; i++) {
-            
+        dp[1][1] = arr[1][1];
+        
+        for (int i=2; i<=N; i++) {
+            dp[0][i] = max(dp[1][i-1], dp[1][i-2]) + arr[0][i];
+            dp[1][i] = max(dp[0][i-1], dp[0][i-2]) + arr[1][i];
         }
+        
+        cout << max(dp[0][N], dp[1][N]) << endl;
     }
 }
