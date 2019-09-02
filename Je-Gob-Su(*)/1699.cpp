ea38c2a658a4fd73477da82036c5a369d21aeb32
Lee Sun Hong <nuseehr@naver.com>
2019-09-03 01:51:03 +0900

    Update Je-Gob-Su problem

--- Je-Gob-Su(*)/1699.cpp
+++ Je-Gob-Su(*)/1699.cpp
@@ -11,7 +11,7 @@ int main() {
     for(int i=1; i<=n ;i++){
         dp[i] = i;
         for(int j=1; j*j<=i; j++)
-            dp[i] = min(dp[i], dp[i-j*j]+1);
+            dp[i] = min(dp[i], dp[i-j*j] + 1);
     }
     
     for (int i=1; i<=n; i++) {
