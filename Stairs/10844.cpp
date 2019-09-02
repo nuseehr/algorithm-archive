711f04b7894695d47b8a14fddeae4034986b2fe9
Lee Sun Hong <nuseehr@naver.com>
2019-09-02 23:58:44 +0900

    Update Stairs Problem

--- Stairs/10844.cpp
+++ Stairs/10844.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 using namespace std;
 
-
 int main()
 {
-
-    int n;
-    int dp[101][10] = {};
+    int dp[101][10] = {0, };
     int sum = 0;
+    int n;
     cin >> n;
     
     for (int i = 0; i < 10; i++)
         dp[1][i] = 1;
-    for (int i = 2; i <= n; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
+    
+    for (int i = 2; i <= n; i++) {
+        for (int j = 0; j < 10; j++) {
             if (j == 0)
                 dp[i][0] = dp[i - 1][1];
             else if (j == 9)
@@ -24,6 +21,7 @@ int main()
                 dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
         }
     }
+    
     for (int i = 1; i < 10; i++)
         sum = (sum + dp[n][i]) % 1000000000;
     
