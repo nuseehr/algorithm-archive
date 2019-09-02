0f0b8e256d6f5fd8117b84eb5ec73a533df07dfe
Lee Sun Hong <nuseehr@naver.com>
2019-09-02 23:59:33 +0900

    Update Stairs Problem

--- Stairs/10844.cpp
+++ Stairs/10844.cpp
@@ -13,12 +13,9 @@ int main()
     
     for (int i = 2; i <= n; i++) {
         for (int j = 0; j < 10; j++) {
-            if (j == 0)
-                dp[i][0] = dp[i - 1][1];
-            else if (j == 9)
-                dp[i][9] = dp[i - 1][8];
-            else
-                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
+            if (j == 0) dp[i][0] = dp[i - 1][1];
+            else if (j == 9) dp[i][9] = dp[i - 1][8];
+            else dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
         }
     }
     
