92b3b04446b3d74e574b9205df1b114b7ca01765
Lee Sun Hong <nuseehr@naver.com>
2019-09-08 21:19:18 +0900

    Add LCM Problem

---
+++ Math/LCM/1934.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int r = a%b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    return a*b / gcd(a, b);
+}
+
+int main() {
+    int testCases;
+    cin >> testCases;
+    
+    while (testCases--) {
+        int a, b;
+        
+        cin >> a >> b;
+        cout << lcm(a, b) << endl;
+    }
+}
