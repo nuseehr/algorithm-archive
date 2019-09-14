69f9813ca3b6b6493707ffd4c30f0974486a9053
Lee Sun Hong <nuseehr@naver.com>
2019-09-14 23:57:36 +0900

    Update Next-Permutation

--- Brute-Force/Next-Permutation(X)/10972.cpp
+++ Brute-Force/Next-Permutation(X)/10972.cpp
@@ -1,18 +1,42 @@
-#include <isotream>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int arr[10001];
+int arr[10002];
+
+void prnt(int N) {
+    for (int i=1; i<=N; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
 int main() {
     int N;
     cin >> N;
-    for (int i=0; i<N; i++) cin >> arr[i];
-    
-    for (int i=N-1; i>0; i--) {
-        if (arr[i-1] < arr[i] ) {
+    for (int i=1; i<=N; i++) cin >> arr[i];
+    while (true) {
+        
+        for (int i=N; i>1; i--) {
+            if (arr[i] > arr[i-1]) {
+                for (int j=N; j>i-1; j--) {
+                    if (arr[j] > arr[i-1]) {
+                        int t = arr[i-1];
+                        arr[i-1] = arr[j];
+                        arr[j] = t;
+                        break;
+                    }
+                    else continue;
+                }
+                sort(arr + i, arr + N+1);
+                break;
+            }
+            if (i == 2) {
+                cout << -1 << endl;
+                return 0;
+            }
         }
+        prnt(N);
     }
-    
-    
 }
