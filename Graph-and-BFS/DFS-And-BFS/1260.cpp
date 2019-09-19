d3e55813dc4987cd1ea92873f76294abcb6fe130
Lee Sun Hong <nuseehr@naver.com>
2019-09-19 15:21:01 +0900

    Add DFS-AND-BFS PROBLEM SOLVED

--- Graph-and-BFS/DFS-And-BFS/1260.cpp
+++ Graph-and-BFS/DFS-And-BFS/1260.cpp
@@ -1,43 +1,65 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
 
-vector<int> g[1001];
+int N, M, V;
+vector<int> e[1001];
 bool visited[1001];
 
-int N, M, V, a, b;
-
-bool dfs(int depth, int crnt) {
-    cout << crnt << " ";
+void dfs(int n) {
     
-    if (depth == M) return true;
+    cout << n << " ";
     
-    visited[crnt] = true;
-    
-    for (int i=0; i<g[crnt].size(); i++) {
-        int next = g[crnt][i];
-        if (visited[crnt]) continue;
-        if (dfs(depth + 1, next)) return true;
+    for (int i=0; i<e[n].size(); i++) {
+        int next = e[n][i];
+        if (!visited[next]) {
+            visited[next] = true;
+            dfs(e[n][i]);
+        }
     }
+}
+
+void bfs() {
+    queue<int> q;
+    q.push(V);
     
-    visited[crnt] = false;
-    
-    return false;
+    visited[V] = 1;
+    while (!q.empty()) {
+        int n = q.front();
+        q.pop();
+        
+        cout << n << " ";
+        for (int i=0; i<e[n].size(); i++) {
+            if (!visited[e[n][i]]) {
+                visited[e[n][i]] = true;
+                q.push(e[n][i]);
+            }
+        }
+    }
 }
 
 int main() {
     
     cin >> N >> M >> V;
     
+    int a, b;
     for (int i=0; i<M; i++) {
-        cin >> a >> b;
-        g[a].push_back(b);
-        g[b].push_back(a);
+        scanf("%d %d", &a, &b);
+        e[a].push_back(b);
+        e[b].push_back(a);
     }
     
-    dfs(1, V);
+    for (int i=1; i<=N; i++) sort(e[i].begin(), e[i].end());
     
+    visited[V] = true;
+    dfs(V);
     cout << "\n";
     
+    for (int i=1; i<=N; i++) visited[i] = false;
+    
+    bfs();
+    cout << "\n";
 }
