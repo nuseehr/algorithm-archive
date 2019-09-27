5772ec5f423c078d0fc59487307ea759973f4205
Lee Sun Hong <nuseehr@naver.com>
2019-09-27 17:05:27 +0900

    Add Good-Word 문제에서 스택의 활용과 string, char 제대로 알기

---
+++ Data-Structure/Good-Word(**)/3986.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int answer = N;
+    string str;
+    
+    while (N--) {
+        stack<char> s;
+        cin >> str;
+        char cur, st;
+        
+        for (int i=0; i<str.length(); i++) {
+            cur = str[i];
+            // str[i]의 타입은 string이 아닌 char.
+            
+            if (!s.empty()) {
+                st = s.top();
+                if (cur == st) s.pop();
+                else s.push(cur);
+            }
+            
+            else s.push(cur);
+        }
+        
+        if (!s.empty()) answer--;
+    }
+    cout << answer << endl;
+}
