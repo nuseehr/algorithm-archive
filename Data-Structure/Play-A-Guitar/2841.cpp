039e054204535a7faf33f4bfac977dba98f53dc8
Lee Sun Hong <nuseehr@naver.com>
2019-09-27 23:05:31 +0900

    Add Guitar Problem 스택 EMPTY 문제 조치방법 확인

---
+++ Data-Structure/Play-A-Guitar/2841.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+stack<int> s[7];
+
+int main() {
+    int N, P, line, prat, cnt=0;
+    cin >> N >> P;
+    for (int i=0; i<7; i++) s[i].push(0);
+    // empty 방지를 위해 초기화.
+    
+    while (N--) {
+        cin >> line >> prat;
+        
+        while (s[line].top() > prat) {
+            s[line].pop();
+            cnt++;
+        }
+        if (s[line].top() < prat) {
+            s[line].push(prat);
+            cnt++;
+        }
+        else if (s[line].top() == prat) {
+            continue;
+        }
+    }
+    cout << cnt << endl;
+}
+
