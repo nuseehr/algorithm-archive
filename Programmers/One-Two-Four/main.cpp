bf09dddf845a6538c8e6d22e91c1a78185743191
Lee Sun Hong <nuseehr@naver.com>
2019-10-12 14:59:46 +0900

    Add One-Two-Four Problem

---
+++ Programmers/One-Two-Four/main.cpp
@@ -0,0 +1,26 @@
+/*
+ 
+ 프로그래머스 - 124 나라의 숫자.
+ 
+ */
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    int tmp = 0;
+
+    while (n > 0) {
+        tmp = n % 3;
+        if (tmp == 0) n = (n / 3) - 1;
+        else n /= 3;
+
+        answer = "412"[tmp] + answer;
+    }
+
+    return answer;
+}
