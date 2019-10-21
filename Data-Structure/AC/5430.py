2e6d38ef222902523bf6bf4975a6d7553f013d3c
Lee Sun Hong <nuseehr@naver.com>
2019-10-21 14:48:43 +0900

    Add python AC PRoblem

---
+++ Data-Structure/AC/5430.py
@@ -0,0 +1,44 @@
+import sys
+
+
+def main():
+    t = int(input())
+
+    while t:
+        cmd = input()
+        length = int(input())
+        arr = sys.stdin.readline().rstrip()
+        arr = arr[1:-1].split(',')
+        rvs = False
+        e = True
+        frnt = 0
+        rear = 0
+
+        for c in cmd:
+            try:
+                if c == 'R':
+                    rvs = not rvs
+                elif c == 'D' and not rvs:
+                    frnt += 1
+                elif c == 'D' and rvs:
+                    rear += 1
+            except:
+                e = False
+                print('error')
+                break
+
+        if e:
+            if frnt + rear <= length:
+                if not rvs:
+                    arr = arr[frnt:length-rear]
+                    print(str(arr).replace('\'', '').replace(' ',''))
+                else:
+                    arr = arr[::-1][rear:length-frnt]
+                    print(str(arr).replace('\'', '').replace(' ',''))
+
+            else:
+                print('error')
+
+        t-=1
+
+main()
