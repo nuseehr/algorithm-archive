720dd15765102bfca98a8008286c7e5ed37c049d
Lee Sun Hong <nuseehr@naver.com>
2019-10-29 00:27:42 +0900

    Add Python Je-Gob-Su Problem

---
+++ Dynamic-Programming/Je-Gob-Su(* KnapSack)/1699.py
@@ -0,0 +1,13 @@
+n = int(input())
+dp = [0 for _ in range(n+1)]
+
+for i in range(1, n+1):
+    dp[i] = i
+    j = 1
+    while j*j <= i:
+        if dp[i] > dp[i-j*j]+1:
+            dp[i] = dp[i-j*j]+1
+        j += 1
+print(dp[n])
+
+ 
