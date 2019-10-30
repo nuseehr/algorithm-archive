6887f72644d9e27381865e09400161fdca628720
Lee Sun Hong <nuseehr@naver.com>
2019-10-30 16:12:26 +0900

    Add Python Lets-Move Problem

---
+++ Dynamic-Programming/Lets-Move/11048.py
@@ -0,0 +1,14 @@
+import sys
+n, m = map(int, input().split())
+
+arr = [[0 for _ in range(m+1)] for _ in range(n+1)]
+dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+
+for i in range(1, n+1):
+    arr[i] = [0] + list(map(int, sys.stdin.readline().split()))
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        dp[i][j] = max(max(dp[i-1][j-1], dp[i-1][j]), dp[i][j-1]) + arr[i][j]
+
+print(dp[n][m])
