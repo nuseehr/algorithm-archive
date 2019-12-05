81a6c56d5825ff92fea8df66c044d8818e393698
Lee Sun Hong <nuseehr@naver.com>
2019-12-05 17:41:49 +0900

    Add Self-Number Problem

--- Basic/Self-Number(Eratos)/4673.cpp
+++ Basic/Self-Number(Eratos)/4673.cpp
@@ -1,35 +1,36 @@
+
 /*
- 소수 = 1과 자신만을 약수로 가지는 수.
- -> X 배수가 될 수가 없음.
- 
- 2부터 증가하며 배수를 체크해 소수가 아닌 수를 제거한다.
- 하지만 하나씩 체크하기엔 너무 시간이 오래걸린다.
- -> 에라토스테네스의 방법을 쓴다. (중복을 생각한다. '제곱'을 이용.)
+ n, d(n), d(d(n)), d(d(d(n))), ... 을
+ for 문으로 정의하였다.
  */
 
-
 #include <iostream>
-#include <vector>
 #include <cmath>
 using namespace std;
+
+int mx = 10001;
+bool isSelfNum[10001];
+
+int tryCheck(int n) {
+    int sum = n;
+    while (true) {
+        if (n == 0) break;
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main() {
  
-int main(){
-    int start, num;
-    scanf("%d %d", &start, &num);
-    vector<bool> prime(num+1, true);
- 
-    for (int i=2; i<=sqrt(num); i++) {
-        if (prime[i]) {
-            for (int j=i+i; j<=num; j+=i) {
-                prime[j] = false;
-            }
+    for (int i=1; i<mx; i++) {
+        for (int j=tryCheck(i); j<mx; j=tryCheck(j)) {
+            if (isSelfNum[j]) continue;
+            isSelfNum[j] = true;
         }
     }
-    
-    for (int i=start; i<num+1; i++) {
-        if (prime[i] == true) {
-            printf("%d\n", i);
-        }
+    for (int i=1; i<mx; i++) {
+        if (!isSelfNum[i]) printf("%d\n", i);
     }
  
 }
