e25d6fd6b4ffbb7e62f3f437aa9a68508155241d
Lee Sun Hong <nuseehr@naver.com>
2019-12-05 16:22:54 +0900

    Add Eratos-Prime Problem 에라토스, 배열동적할당

---
+++ Basic/Eratos-Prime/1929.cpp
@@ -0,0 +1,37 @@
+/*
+ 소수 = 1과 자신만을 약수로 가지는 수.
+ -> X 배수가 될 수가 없음.
+ 
+ 2부터 증가하며 배수를 체크해 소수가 아닌 수를 제거한다.
+ 하지만 하나씩 체크하기엔 너무 시간이 오래걸린다.
+ -> 에라토스테네스의 방법을 쓴다. (중복을 생각한다. '제곱'을 이용.)
+ */
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+ 
+int main(){
+    int start, num;
+    scanf("%d %d", &start, &num);
+    int *prime = new int[num+1]();
+    
+    prime[1] = false;
+ 
+    for (int i=2; i<=sqrt(num); i++) {
+        if (!prime[i]) {
+            for (int j=i+i; j<=num; j+=i) {
+                prime[j] = true;
+            }
+        }
+    }
+    
+    for (int i=start; i<num+1; i++) {
+        if (prime[i] == false) {
+            printf("%d\n", i);
+        }
+    }
+ 
+    delete[] prime;
+}
