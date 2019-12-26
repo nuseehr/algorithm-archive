78b568ed728f64160f4bcc2f8b4a91ac265c1e75
Lee Sun Hong <nuseehr@naver.com>
2019-12-26 21:08:58 +0900

    Add Sorting-Number-2

---
+++ Basic/Sorting-Number-2/2751.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    int* arr = new int[N];
+    
+    for (int i=0; i<N; i++) scanf("%d", &arr[i]);
+    
+    sort(arr, arr+N);
+    for (int i=0; i<N; i++) printf("%d\n", arr[i]);
+    
+    delete[] arr;
+}
