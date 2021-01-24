afb1cc69b6df8910d6bd2e4fb05eae8e8532f270
Sun Hong Lee <nuseehr@naver.com>
2021-01-24 14:06:47 +0900

    Create main.cpp

---
+++ Programmers/완주하지-못한-선수/main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+string solution(vector<string> p, vector<string> c) {
+    string answer = "";
+    map<string, int> m;
+    
+    
+    for(auto e: p) m[e]++;
+    for(auto e: c) m[e]--;
+    
+    for(auto it=m.begin(); it!=m.end(); it++) {
+        cout << it->second << " " << it->first;
+        if(it->second == 1) answer = it->first;
+    }
+    
+    return answer;
+}
