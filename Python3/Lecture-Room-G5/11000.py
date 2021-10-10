397377e563c67983ef03786b7ab861748a99fb90
nuseehr <nuseehr@naver.com>
2021-10-10 18:22:51 +0900

    Add Greedy 11000 Python Problem G5

--- Python3/Lecture-Room-G5/11000.py
+++ Python3/Lecture-Room-G5/11000.py
@@ -1,42 +1,29 @@
-
 import sys
-from queue import PriorityQueue
+import heapq
 
 N = int(sys.stdin.readline())
-Room = PriorityQueue()
+TimeTable = []
 
+# 힙으로 사용할 배열을 선언한다. - 최소 힙.
+Room = []
+
+# 강의 시작시간과 종료시간을 TimeTable 배열에 담는다
 for i in range(N):
     S, T = map(int, sys.stdin.readline().split())
-    Room.put((S, T))
+    TimeTable.append([S, T])
 
-Mn = []
+# 시작시간을 기준으로 정렬한다.
+TimeTable.sort(key=lambda x:x[0])
+heapq.heappush(Room, TimeTable[0][1])
 
-for i in range(Room.qsize()):
-    Time = Room.get()
-    if i == 0:
-        Mn.append(Time[1])
-    else:
-        Start = min(Mn)
-        if Time[0] >= Start:
-            del Mn[Mn.index(Start)]
-            Mn.append(Time[1])
-        else:
-            Mn.append(Time[1])
-
-print(len(Mn))
-
-
-'''
-print(Room)
-MN = Room.get()[1]
-CNT = 1
-
+# 강의실의 수를 담을 Room에 강의 종료 시간을 넣어준다.
+# 힙 안에 있는 최소 힙의 강의 종료시간보다 TimeTable 배열 안에서
+# 이른 시간이 나오면 그것은 강의실이 하나 더 필요함을 나타낸다.
 for i in range(1, N):
-    print(Room()[0], MN)
-    if Room[i][0] >= MN:
-        MN = Room[i][0]
+    if TimeTable[i][0] < Room[0]:
+        heapq.heappush(Room, TimeTable[i][1])
     else:
-        CNT += 1
-
-print(CNT)
-'''
+        heapq.heappop(Room)
+        heapq.heappush(Room, TimeTable[i][1])
+        
+print(len(Room))
