3f56e699e8e534110b94f69d66206cb26e62e3a4
nuseehr <nuseehr@naver.com>
2021-10-14 18:09:56 +0900

    Update Comment Python Dp Wine Problem

--- Python3/Wine-S1/2156.py
+++ Python3/Wine-S1/2156.py
@@ -11,13 +11,23 @@ for i in range(N):
     
 if N == 1:
     print(Wines[1])
-elif N == 2:
-    print(Wines[1] + Wines[2])
 else:
     Dp[1] = Wines[1]
     Dp[2] = Wines[1] + Wines[2]
     for i in range(3, N+1):
-        Dp[i] = max(Dp[i-3] + Wines[i] + Wines[i-1], Dp[i-2] + Wines[i])
-        Dp[i] = max(Dp[i-1], Dp[i])
+    
+        '''
+        Case 1. i번째 포도주를 마신다. 그리고 i-1번째 포도주를 마시고 i-2번째 포도주를 마시지 않는다.
+        Case 2. i번째 포도주를 마신다. 그리고 i-2번째 포도주를 마시고 i-1번째 포도주를 마시지 않는다.
+        Case 3. i번째 포도주를 마시지 않는다.
+        '''
+        
+        Case1 = Wines[i] + Wines[i-1] + Dp[i-3]
+        Case2 = Wines[i] + Dp[i-2]
+        Case3 = Dp[i-1]
+        
+        # Dp[i]의 값은 i번째 와인까지의 마시는 와인의 최대값이다.
+        Dp[i] = max(Case1, Case2, Case3)
 
-    print(Dp)
+    print(Dp[N])
+    
