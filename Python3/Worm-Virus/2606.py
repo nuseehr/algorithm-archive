8ae726698edf91ab54922e59b0878d0e06cad75c
nuseehr <nuseehr@naver.com>
2021-10-21 22:57:04 +0900

    Update Python DFS 2606 Problem

--- Python3/Worm-Virus/2606.py
+++ Python3/Worm-Virus/2606.py
@@ -1,16 +1,16 @@
 
 import sys
 
-def DFS(Node, Cnt):
+def DFS(Node):
     Seen[Node] = 1
 
     for i in range(1, N+1):
         if Seen[i] == 0 and Graph[Node][i] == 1:
-            DFS(i, Cnt+1)
+            DFS(i)
+            Computers.append(i)
+            
+    return len(Computers)
     
-    Computers.add(Node)
-            
-            
 N = int(sys.stdin.readline())
 M = int(sys.stdin.readline())
 
@@ -22,9 +22,6 @@ for i in range(M):
     Graph[a][b] = Graph[b][a] = 1
 
 Seen = [0] * (N+1)
-Cnt = 1
-Computers = set()
+Computers = []
 
-DFS(1, Cnt)
-Computers.remove(1)
-print(len(Computers))
+print(DFS(1))
