36680da53da7d710ec40bddd5ec2c9f4d4e5beeb
nuseehr <nuseehr@naver.com>
2021-11-01 22:31:53 +0900

    Edit Python3 1969 String Problem

--- Python3/DNA-S5/1969.py
+++ Python3/DNA-S5/1969.py
@@ -9,6 +9,8 @@ Cnt = 0
 for i in range(N):
     Dna.append(sys.stdin.readline().rstrip())
     
+# ord 함수는 문자를 ASCII 코드 숫자로 치환함.
+# chr 함수는 ASCII코드를 문자로 치환함.
 for i in range(M):
     Alphabet = [0 for _ in range(26)]
     for j in range(N):
