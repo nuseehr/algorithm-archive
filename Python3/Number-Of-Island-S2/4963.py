aed8b089ea384e68915e701d98f9f79edc66c90c
nuseehr <nuseehr@naver.com>
2021-11-06 20:59:11 +0900

    Update Python 4963 DFS Problem

--- Python3/Number-Of-Island-S2/4963.py
+++ Python3/Number-Of-Island-S2/4963.py
@@ -1,16 +1,13 @@
 import sys
+# 파이썬 재귀 문제 해결시 필수 추가해 줄 것.
 sys.setrecursionlimit(100000)
 
 dx = [-1, 1, 0, 0, -1, 1, -1, 1]
 dy = [0, 0, 1, -1, 1, 1, -1, -1]
 
-def DFS(y, x, Cnt):
+def DFS(y, x):
     Seen[y][x] = 1
-    # print('[', y, ',', x, ']')
-    '''
-    for i in range(H):
-        print(Seen[i])
-    '''
+
     global Num
     
     if Graph[y][x] == 1:
@@ -22,13 +19,11 @@ def DFS(y, x, Cnt):
         
         if 0 <= ny < H and 0 <= nx < W:
             if Seen[ny][nx] == 0 and Graph[ny][nx] == 1:
-                # print('CALL DFS [', ny, ',', nx, ']')
-                DFS(ny, nx, Cnt)
+                DFS(ny, nx)
     return Num
     
 while True:
     W, H = map(int, sys.stdin.readline().split())
-    Cnt = 1
     Num = 0
     Ans = []
     
@@ -44,6 +39,6 @@ while True:
         for y in range(H):
             for x in range(W):
                 if Graph[y][x] == 1 and Seen[y][x] == 0:
-                    Ans.append(DFS(y, x, Cnt))
+                    Ans.append(DFS(y, x))
                     Num = 0
         print(len(Ans))
